2c2480590b868925944599a15ab1eac1f38f682a
aeyyjeyyaryan <mr.aryanjha@gmail.com>
2023-08-26 18:56:53 +0545

    done

---
+++ #code to create calculator.py
@@ -0,0 +1,8 @@
+#code to create calculator
+p = eval(input("enter the value of p: "))
+t = eval(input("enter the value of t: "))
+r = eval(input("enter the vale of r: "))
+si = (p*t*r/100)
+print(si)
+
+
