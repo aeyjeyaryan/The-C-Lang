19a22379277f2b32d8ff6cf4e3ed005d90f71aa8
aeyyjeyyaryan <mr.aryanjha@gmail.com>
2023-08-26 18:18:17 +0545

    python calculator

--- Simple Interest Calculator in Python.py
+++
@@ -1,7 +0,0 @@
-Simple Interest Calculator
-principal = eval(input("Enter the principal amount: "))
-rate_of_interest = eval(input("Enter the rate of interest (in percentage): "))
-time_period = eval(input("Enter the time period (in years): "))
-simple_interest = (principal * rate_of_interest * time_period) / 100
-print(f"Simple Interest: {simple_interest}")
-
