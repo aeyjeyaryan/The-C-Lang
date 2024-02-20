6b71335085fec980878692beadf9682d0f26a3ca
aeyyjeyyaryan <mr.aryanjha@gmail.com>
2024-02-20 09:45:44 +0530

    A program to calculate area of rectangle

--- #code to create calculator.py
+++
@@ -1,8 +0,0 @@
-#code to create calculator
-p = eval(input("enter the value of p: "))
-t = eval(input("enter the value of t: "))
-r = eval(input("enter the vale of r: "))
-si = (p*t*r/100)
-print(si)
-
-
