c4f4e8722068a2a4df8288d2bf99e2938ff9660c
Aryan Kumar Jha <143257866+aeyjeyaryan@users.noreply.github.com>
2024-03-09 16:32:00 +0530

    Loops part 2

--- Loops.c
+++ Loops.c
@@ -1,75 +1,48 @@
-// #include<stdio.h>
-// int main()
-// {
-//     for(int i=1; i<5; i=i+1){
-//         printf("hello world \n");
-//     }
-//     return 0;
+// LOOPS PART 2
+// DO WHILE
+// #include <stdio.h>
+// int main(){
+//  int i = 1;
+//  do{
+//     printf("%d\n",i);
+//     i++;
+//  } while (i<=5);
+//  return 0; 
 // }
-//using loops with numbers
-// #include<stdio.h>
-// int main()
-// {
-//     for(int i=1; i<=100; i++){
-//         printf("%d \n", i);
-//     }
-//     return 0;
-// }
-
-//print numbers from 0 to 10
-// #include<stdio.h>
-// int main()
-// {
-//     for(int i=0; i<=10; i++){  //i++ follows use then increase
-//         printf("%d", i);
-//     }
-//     return 0;
-// }
-// #include<stdio.h>
-// int main()
-// {
-//   int i = 1;
-//   printf("%d\n",++i);
-//   printf("%d",i); //++i follows increase then use
-
-//     return 0;
-// }
-
-//WHILE LOOP//
-// #include<stdio.h>
-// int main()
-// {
-//     int i=1;
-//     while(i<=5) {
-//         printf("ye kya hogaya bhai!\n");
-//         i++;
-//     }
-
-// return 0;
-// }
-//PRINT THE NUMBERS FORM O TO N, IF N IS GIVEN BY USER
-// #include<stdio.h>
-// int main()
-// {
-//     int n;
-//     int i = 0;
-//     printf("enter the value of n: ");
-//     scanf("%d",&n);
-//     while(i<=n){
-//         printf("%d",i);
-//         i++;
-//     }
-// return 0;
-// }
-// do while basic code
-#include<stdio.h>
+ // print the sum of first n natural numbers
+ // also print them in reverse
+#include <stdio.h>
 int main()
 {
-    int i = 1;
-    do {
-        printf("%d\n", i);
-        i++;
-    } while(i <= 5);
+    int n = 5;
+    int sum = 0;
+    for(int i=1; i<=n; i++){
+        sum = sum + i;
+    }
+    printf("%d\n",sum);
+    for (int i=n; i>=1; i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}    
 
+// print factorial of a number
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+
+    printf("%d" ,factorial);
     return 0;
 }
+
+     
