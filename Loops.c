b2090824d6ff81a3455d532ce40eca7c9e22d4ab
Aryan Kumar Jha <143257866+aeyjeyaryan@users.noreply.github.com>
2024-03-09 16:11:34 +0530

    basics of lopps

---
+++ Loops.c
@@ -0,0 +1,74 @@
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1; i<5; i=i+1){
+//         printf("hello world \n");
+//     }
+//     return 0;
+// }
+//using loops with numbers
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1; i<=100; i++){
+//         printf("%d \n", i);
+//     }
+//     return 0;
+// }
+
+//print numbers from 0 to 10
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=0; i<=10; i++){  //i++ follows use then increase
+//         printf("%d", i);
+//     }
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//   int i = 1;
+//   printf("%d\n",++i);
+//   printf("%d",i); //++i follows increase then use
+
+//     return 0;
+// }
+
+//WHILE LOOP//
+// #include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=5) {
+//         printf("ye kya hogaya bhai!\n");
+//         i++;
+//     }
+
+// return 0;
+// }
+//PRINT THE NUMBERS FORM O TO N, IF N IS GIVEN BY USER
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     int i = 0;
+//     printf("enter the value of n: ");
+//     scanf("%d",&n);
+//     while(i<=n){
+//         printf("%d",i);
+//         i++;
+//     }
+// return 0;
+// }
+#include<stdio.h>
+int main()
+{
+    
+
+
+
+
+
+    return 0;
+}
