a0680a7826cb30e9b6c58dc5909412ba02412a70
Aryan Kumar Jha <143257866+aeyjeyaryan@users.noreply.github.com>
2024-03-11 21:35:28 +0530

    Update Functions and recursion.c

--- Functions and recursion.c
+++ Functions and recursion.c
@@ -14,3 +14,25 @@ int main() {
 void greet() {
     printf("Hello, world!\n");
 }
+
+// addition function 
+#include <stdio.h>
+
+// Function declaration
+int add(int a, int b);
+
+int main() {
+    int num1 = 5, num2 = 10;
+    
+    // Function call
+    int sum = add(num1, num2);
+    
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
